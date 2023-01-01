c85aedbb3c882dd6f23b6b45ca575797a84d4689
MrMiquy <mrmiquy@gmail.com>
2023-01-01 23:03:10 +0100

    finally setup gtk4

---
+++ Sources/main.c
@@ -0,0 +1,23 @@
+#include <gtk/gtk.h>
+
+static void
+app_activate (GApplication *app, gpointer user_data) {
+  GtkWidget *win;
+
+  win = gtk_application_window_new (GTK_APPLICATION (app));
+  gtk_window_set_title (GTK_WINDOW (win), "ydere");
+  gtk_window_set_default_size (GTK_WINDOW (win), 400, 300);
+  gtk_widget_show (win);
+}
+
+int
+main (int argc, char **argv) {
+  GtkApplication *app;
+  int stat;
+
+  app = gtk_application_new ("com.github.MrMiquy.ydere", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect (app, "activate", G_CALLBACK (app_activate), NULL);
+  stat =g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+  return stat;
+}
